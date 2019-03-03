6fccd21c88de04c8c878e01e97007513502c649b
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-03 01:04:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,14 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_pop(array, string)
-  array.pop(string)
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
 end
