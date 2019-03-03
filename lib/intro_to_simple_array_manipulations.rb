a58408328a9c2d1ecbb26fbc4b83691b3a205104
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-03 01:14:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,15 +19,15 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  
+  array.shift(2)
 end
 
 def using_concat(array,array2 )
-  
+  array.concat(array2)
 end
 
 def using_insert(array, element)
-  array.insert(element)
+  array[4].insert(element)
 end
 
 def using_uniq(array)
